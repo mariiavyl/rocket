fa0b6fa96fcc5d68906a53bb313a4f3d40a823e0
marivyl <103947891+marivyl@users.noreply.github.com>
2024-06-05 11:36:57 +0300

    final

--- js/rocket.js
+++ js/rocket.js
@@ -10,10 +10,20 @@ if (state == 2) {
     timer = setInterval(function(){
         countdownNumber = countdownNumber-1;
         document.getElementById('countdown').innerHTML = countdownNumber;
+        if (countdownNumber > 4 && countdownNumber <= 7){
+            document.getElementById('nervous').className = 'nervous show';
+        } else{
+            document.getElementById('nervous').className = 'nervous';
+        };
+        if (countdownNumber > 1 && countdownNumber <= 4){
+            document.getElementById('cant-wait').className = 'cant-wait show';
+        } else{
+            document.getElementById('cant-wait').className = 'cant-wait'; 
+        }
         if (countdownNumber<=0){
             changeState(3);
-        }
-    }, 500);
+        };
+    }, 1000);
 } else if (state == 3){
 var success = setTimeout(function()
 {
